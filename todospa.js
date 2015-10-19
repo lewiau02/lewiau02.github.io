c8a933bfc7192e2ccec9a83146a19947e2212d7f
lewiau02 <lewiau02@luther.edu>
2015-10-19 14:12:16 -0500

    Create todospa.js

---
+++ todospa.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+quizFunc = function() {
+           newli = document.createElement("li");
+           newin = document.createElement("input");
+           myPriority = document.querySelector("#priority")
+           newli.className = myPriority.value
+           newIn.type = "checkbox"
+           mynewli = document.createTextNode()
+           boxChecked= function()
+           this.parentNode.className = "done"
+         }
