8fca1b0db0b0cdfc682f6655c2337cd5c304546e
lewiau02 <lewiau02@luther.edu>
2015-10-19 15:15:42 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -1,5 +1,6 @@
 quizFunc = function() {
            newli = document.createElement("li");
+           newul = document.createElement("ul");
            newin = document.createElement("input");
            mynewli = document.createTextNode()
            boxChecked= function()
@@ -7,5 +8,16 @@ quizFunc = function() {
            myPriority = document.querySelector("#priority")
            newli.className = myPriority.value
            newIn.type = "checkbox"
+           newli.appendChild(mynewli);
+           ul.appendChild(newli);
+           document.header.appendChild(ul);
+           li1.appendChild(myli1);
+      
+           li2.appendChild(myli2);
+           p.appendChild(myp);
+           h1.appendChild(myh1);
+      document.body.appendChild(h1);
+      document.body.appendChild(p);
+      document.body.appendChild(ol);
          }
 
