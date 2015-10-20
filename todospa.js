1ae22b308f0972f570c140f089d836eaab67412e
lewiau02 <lewiau02@luther.edu>
2015-10-20 14:20:43 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -4,12 +4,13 @@ quizFunc = function() {
            myTasklist = document.querySelector("#tasklist")
            myPriority = document.querySelector("#priority")
            mynewli = document.createTextNode()
-           boxChecked= function()
-           this.parentNode.className = "done"
            newli.className = myPriority.value
            newIn.type = "checkbox"
            newli.appendChild(mynewli);
            ul.appendChild(newli);
            document.body.appendChild(ul);
+}
+           boxChecked= function() {
+           this.parentNode.className = "done"
          }
 
